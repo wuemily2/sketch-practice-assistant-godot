b75796a840120e56d2a4e7dcd996762e77f7a82a
Emily Wu <chinadragon.emily@gmail.com>
2024-01-31 21:34:30 -0500

    Modify test files

---
+++ Windows/Container.gd
@@ -0,0 +1,15 @@
+extends Container
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _notification(what):
+	if what == NOTIFICATION_SORT_CHILDREN:
+		print("Container SORT!")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- Windows/ImageScalingTest.gd
+++ Windows/ImageScalingTest.gd
@@ -1,14 +1,27 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
+# Solved window resize signaling issue with 
+# https://github.com/godotengine/godot/issues/79336#issuecomment-1631627181
 func _ready():
 	var vwindow:Window = get_window()
 	print('_ready(): connecting size_changed to on_size_changed')
 	vwindow.size_changed.connect(on_size_changed)
+	self.position.x = 0
+	self.position.y = 0
 
 func on_size_changed():
 	print("size changed!")
+	# Expand size to fit screen, and fill 80% of the height
+	self.size.x = get_viewport().size.x
+	self.size.y = get_viewport().size.y
+	#$HBoxContainer.size.y = self.size.y * 0.5
+	#$HBoxContainer.size.x = self.size.x * 0.5
+	#for child in get_children():
+		#if child is Container:
+			#child.queue_sort()
+	
+	
 
 func _notification(what):
 	if what == NOTIFICATION_WM_SIZE_CHANGED:
